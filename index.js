b009b8ca388c7de42b49f3f712cbef13c5f0d255
JosMigue <jojo_miguel20@hotmail.com>
2025-07-21 18:46:43 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+//const authRoutes = require('./routes/auth');
+
+app.use(express.json());
+
+app.use('/api', (req, res) => {
+    res.send({
+        message: 'Welcome to the API!'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
